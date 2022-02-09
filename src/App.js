0dbe607e068c0e52d90b6d9a1b5bf4f1811f7532
Chathura Harshanga <38973288+chathuraHarshanga@users.noreply.github.com>
2022-02-09 22:23:54 +0530

    movies fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+
+//import components
+import Movie from "./Movie";
 
 function App() {
-  return <div className="App"></div>;
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=8c5b5e30e504139fd6699ba73c13557e&language=en-US&page=1"
+    );
+    const movies = await data.json();
+    setPopular(movies.results);
+  };
+
+  return (
+    <div className="App">
+      <div className="popular-movies">
+        {popular.map((movie) => {
+          console.log(movie);
+          return <Movie key={movie.id} movie={movie} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
